a2f189509825cbbdb97d89154c9adad9c9ef4a3e
john.hiles <john.hiles@wright.edu>
2021-08-10 21:44:44 -0400

    Added simulator.py, which adds simulator class. Started restructuring main.py

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 import pickle
 import datetime
 
+from stonesoup.types.array import StateVector, CovarianceMatrix
+from stonesoup.types.state import State, GaussianState, EnsembleState
+
+from stonesoup.models.transition.linear import (CombinedLinearGaussianTransitionModel,
+                                                ConstantTurn, ConstantVelocity)
+from stonesoup.models.measurement.nonlinear import CartesianToElevationBearingRange, CartesianToBearingRange
+from stonesoup.models.measurement.linear import LinearGaussian
+
+
+
 from track_functions import generate_ground_truth, \
                             linear_2d_monte_carlo_runs, nonlinear_2d_monte_carlo_runs,monte_carlo_runs_Niu
 from initialization_functions import generate_models_Niu
@@ -10,6 +20,27 @@ from initialization_functions import generate_models_Niu
 from RMSE_functions import calc_RMSE, plot_RMSE
 from MISC_functions import pack_RMSE_data
 
+from simulator import simulator
+
+from stonesoup.updater.ensemble import (EnsembleUpdater, EnsembleSqrtUpdater)
+from stonesoup.updater.kalman import KalmanUpdater, ExtendedKalmanUpdater
+from stonesoup.predictor.ensemble import EnsemblePredictor
+
+'''
+    This script represents the code used to gather the data used in [PAPER HERE].
+    
+    This repository is structured such that different stone soup can be run 
+    relativley rapidly. 
+    
+    The simulator class requires a transition and 
+    measurement model, then the simulate_track method accepts a Stone Soup
+    Predictor, Updater, ground truth initial state, initial state for the
+    chosen algorithm, and a span of time which the simulation takes place over.
+    This time span should be an evenly spaced datetime.datetime list.
+    
+    The simulator then, is used to gather "Track" instances, and with a list 
+    of tracks, RMSE can then be calculated.
+'''
 
 i = 10
 num_vectors = i*5
@@ -25,6 +56,39 @@ time_span = np.array([timestamp + datetime.timedelta(seconds=dt*i) for i in rang
 
 monte_carlo_iterations = 50
 
+
+q_x = 0.05
+q_y = 0.05
+transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(q_x),
+                                                      ConstantVelocity(q_y)])
+measurement_model = LinearGaussian(
+ndim_state=4,  # Number of state dimensions (position and velocity in 2D)
+mapping=(0, 2),  # Mapping measurement vector index to state index
+noise_covar=np.array([[5, 0],  # Covariance matrix for Gaussian PDF
+                      [0, 5]])
+)
+
+simulator = simulator(transition_model=transition_model, measurement_model=measurement_model)
+
+
+predictor = EnsemblePredictor(transition_model)
+updater = EnsembleUpdater(measurement_model)
+
+initial_ground_truth = State(state_vector=StateVector([0, 1, 0, 1]),
+                             timestamp = time_span[0])
+
+EnKFprior = EnsembleState.from_gaussian_state(
+    GaussianState(state_vector=StateVector([0, 1, 0, 1]),
+                  covar = CovarianceMatrix(np.diag(np.array([0,0.05,0,0.05]))**2),
+                  timestamp = time_span[0]), num_vectors)
+
+EnKF_run = simulator.simulate_track(predictor = predictor, 
+                                    updater = updater, 
+                                    initial_state = initial_ground_truth,
+                                    prior = EnKFprior,
+                                    time_span=time_span)
+
+'''
 EKF_runs, EnKF_runs, EnSRF_runs, ground_truth = nonlinear_2d_monte_carlo_runs(time_span, monte_carlo_iterations, num_vectors)
 
 RMSE_EKF = calc_RMSE(ground_truth, EKF_runs)
@@ -43,7 +107,7 @@ for RMSE in RMSE_LIST:
     j = j+1
 
 
-'''
+
 pack_RMSE_data('track_nonlinear_measurement_Ensemble.txt',
 
 with open('rmse_nonlinear_measurement_Ensemble.txt', 'wb') as f:
@@ -53,3 +117,4 @@ with open('track_nonlinear_measurement_Ensemble.txt', 'wb') as g:
     pickle.dump([EnKF_runs, EnSRF_runs], g)
 
 '''
+
---
+++ simulator.py
@@ -0,0 +1,107 @@
+import numpy as np
+import datetime
+
+from stonesoup.types.array import StateVector, CovarianceMatrix
+from stonesoup.types.state import GaussianState, EnsembleState
+
+from stonesoup.models.transition.linear import (CombinedLinearGaussianTransitionModel,
+                                                ConstantTurn, ConstantVelocity)
+from stonesoup.models.measurement.nonlinear import CartesianToElevationBearingRange, CartesianToBearingRange
+from stonesoup.models.measurement.linear import LinearGaussian
+
+
+import numpy as np
+import time
+
+from stonesoup.types.groundtruth import GroundTruthPath, GroundTruthState
+from stonesoup.types.detection import Detection
+from stonesoup.types.array import StateVector, CovarianceMatrix
+
+from stonesoup.types.track import Track
+from stonesoup.predictor.kalman import KalmanPredictor, ExtendedKalmanPredictor
+from stonesoup.predictor.ensemble import EnsemblePredictor
+from stonesoup.types.hypothesis import SingleHypothesis
+from stonesoup.updater.ensemble import (EnsembleUpdater, EnsembleSqrtUpdater)
+from stonesoup.updater.kalman import KalmanUpdater, ExtendedKalmanUpdater
+
+from stonesoup.predictor.particle import ParticlePredictor
+from stonesoup.resampler.particle import SystematicResampler
+from stonesoup.updater.particle import ParticleUpdater
+
+from initialization_functions import generate_initial_states_Niu, generate_models_Niu,\
+                                    generate_linear_models_Niu, generate_2d_linear_models, \
+                                    generate_initial_2d_states
+
+
+class simulator():
+        
+    def __init__(self, transition_model, measurement_model):
+        """
+        Parameters
+        ----------
+        transition_model : :class:`~.Predictor`
+            The Stone Soup predictor to be used.
+        measurement_model : :class:`~.Predictor`
+            The Updater to be used.
+        """
+        self.transition_model= transition_model
+        self.measurement_model = measurement_model        
+    
+    def _generate_ground_truth(self, prior, time_span):
+        
+        time_interval = time_span[1]-time_span[0]
+        ground_truth = GroundTruthPath([prior])
+        for k in range(1, len(time_span)):
+            ground_truth.append(GroundTruthState(
+                self.transition_model.function(ground_truth[k-1], noise=False, time_interval=time_interval),
+                timestamp=time_span[k-1]))
+        return ground_truth
+    
+    def _simulate_measurements(self, ground_truth):
+        #Simulate Measurements
+        measurements = []
+        for state in ground_truth:
+            measurement = self.measurement_model.function(state, noise=True)
+            measurements.append(Detection(measurement,
+                                          timestamp=state.timestamp,
+                                          measurement_model=self.measurement_model))
+        return measurements
+    
+    def simulate_track(self, predictor, updater, initial_state, prior, time_span):
+        """
+
+        Parameters
+        ----------
+        predictor : :class:`~.Predictor`
+            The Stone Soup predictor to be used.
+        updater : :class:`~.Predictor`
+            The Updater to be used.
+        ground_truth : :class:`~.GroundTruthPath`
+            StateMutableSequence type object used to store ground truth.
+        initial_state : :class:`~.State`
+            Initial state for the ground truth system. This MUST be a State,
+            not a State subclass, like GaussianState or EnsembleState.
+        prior : :class:`~.GaussianState` or :class:`~.EnsembleState`
+            Initial state prediction of tracking algorithm.
+
+        Returns
+        -------
+        track : :class:`~.Track`
+            The Stone Soup track object which contains the list of updated 
+            state predictions made by the tracking algorithm employed.
+        """
+        
+        #Simulate Measurements
+        ground_truth = self._generate_ground_truth(initial_state, time_span)
+        measurements = self._simulate_measurements(ground_truth)
+        
+        #Initialize Loop Variables
+        track = Track()
+        for measurement in measurements:
+            prediction = predictor.predict(prior, timestamp=measurement.timestamp)
+            hypothesis = SingleHypothesis(prediction, measurement)  # Group a prediction and measurement
+            posterior = updater.update(hypothesis)
+            track.append(posterior)
+            prior = track[-1]
+        return track
+    
